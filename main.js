b1c448cdc1ccc4122536b5453c7682723ff893cd
Saurav Rawat <rawatsaurav889@gmail.com>
2019-12-29 17:10:28 +0530

    Updates

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
+//jshint esversion:6
 //setting random movies.
-function randomMovies() {
-  typeOfData = document.getElementById('typeOfData').value;
-    axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=06952d5eda1c1b29d20609da5e9d2693`)
+function randomMovies() 
+{
+    axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=06952d5eda1c1b29d20609da5e9d2693`)
       .then((response) => {
         let movies = response.data.results;
       	let output = '';
       	movies.forEach(movie => {
-      	image=`https://image.tmdb.org/t/p/original/${movie.poster_path}`
+      	image=`https://image.tmdb.org/t/p/original/${movie.poster_path}`;
         output += `
+
             <div class='col-md-3'>
             <div class= 'well text-center'>
                 <img src='${image}'></img>
-                <h6>${movie.title}</h6>
+                <h4>${movie.title}</h4>
             <button onclick= "movieId('${movie.id}')" class="btn btn-primary" href="#">Movie Detail</button>
             </div>
             </div>`;
@@ -21,26 +23,22 @@ function randomMovies() {
       .catch((err) => {
         console.log(err);
       });
-
 }
-
 function getMovie() {
   let searchText;
-  let typeOfData;
   searchText = document.getElementById('searchText').value;
-  typeOfData = document.getElementById('typeOfData').value;
   if(searchText&&typeOfData){
   axios.get(`https://api.themoviedb.org/3/search/movie?api_key=06952d5eda1c1b29d20609da5e9d2693&query=${searchText}`)
     .then((response) => {
       let movies = response.data.results;
       let output = '';
       movies.forEach(movie => {
-      	image=`https://image.tmdb.org/t/p/original/${movie.poster_path}`
+      	image=`https://image.tmdb.org/t/p/original/${movie.poster_path}`;
         output += `
             <div class='col-md-3'>
             <div class= 'well text-center'>
                 <img src='${image}'></img>
-                <h6>${movie.title}</h6>
+                <h4>${movie.title}</h4>
             <button onclick= "movieId('${movie.id}')" class="btn btn-primary" href="#">Movie Detail</button>
             </div>
             </div>`;
@@ -68,44 +66,48 @@ function movieId(id) {
      movie.genres.forEach(obj=>{
 		    	if(genereString===0){
 		    		genereString=`${obj.name}`;
-		    	}else{genereString+=` ,${obj.name}`
+		    	}else{genereString+=` ,${obj.name}`;
 		    	}
 		    		
-    	})
-console.log(movie)
-    let output = `
-         <div class="row">
-           <div class="col-md-4">
-             <img src="${image}" class="thumbnail">
-           </div>
-           <div class="col-md-8">
-             <h2>${movie.original_title}</h2>
-             <ul class="list-group">
-         		
-         		<li class="list-group-item"><strong>Genre:</strong> ${genereString}</li>
-               <li class="list-group-item"><strong>Released:</strong> ${movie.release_date}</li>
-               <li class="list-group-item"><strong>Bundget:</strong> ${movie.budget}</li>
-               <li class="list-group-item"><strong>Revenue:</strong> ${movie.revenue}</li>
-      			<li class="list-group-item"><strong>Status:</strong> ${movie.status}</li>
-      			<li class="list-group-item"><strong>Tagline:</strong> ${movie.tagline}</li>
-             </ul>
-           </div>
-         </div>
+      });
+      
 
-        <div class="row">
-          <div class="well">
-            <h3>Plot</h3>
-            ${movie.overview}
-            <hr>
-            <a href="${movie.homepage}" target="_blank" class="btn btn-primary">View IMDB</a>
-            <a href="index.html" class="btn btn-default">Go Back To Search</a>
-          </div>
+
+
+
+console.log(movie);
+    let output = `
+         
+<div class="card mb-3" >
+  <div class="row no-gutters">
+    <div class="col-md-4">
+    <img src="${image}" class="card-img" alt="...">
+    </div>
+      <div class="col-md-8">
+        <div class="card-body">
+              <h1 class="card-title text-center">${movie.original_title}</h1>
+              <h4 class="card-text">  ${movie.overview}</h4>
+              <h3><strong>Genre:</strong> ${genereString}</br></h3>
+              <h3><strong>Released:</strong> ${movie.release_date}</br></h3>
+              <h3><strong>Budget:</strong> ${movie.budget}</br></h3>
+              <h3><strong>Revenue:</strong> ${movie.revenue}</br></h3>
+              <h3><strong>Status:</strong> ${movie.status}</br></h3>
+              <h3><strong>Tagline:</strong> ${movie.tagline}</br></h3>
         </div>
-      `;
-    $('#movie').html(output);
+      </div>
+    </div>
+</div>
+             
+<div class="row">
+  <div class="well">
+    <a href="${movie.homepage}" target="_blank" class="btn btn-primary">View IMDB</a>
+    <a href="index.html" class="btn btn-default">Go Back To Search</a>
+  </div>
+</div> `;
+$('#movie').html(output);
 
   }).catch((err) => {
-    console.log(err)
+    console.log(err);
   });
 
 }
